5d198fe464a368c60ca94f0b6a63e4688670705d
BOSNYLA <shinylincom@gmail.com>
2020-07-02 14:08:17 +0800

    start with week2

---
+++ homeworks/week1/bin/bash/num.sh
@@ -0,0 +1,12 @@
+#  輸入數字n，然後產生n個js資料
+
+
+for number in ${number}.js
+
+do
+
+touch "${number}.js";
+
+echo "檔案建立完成";
+
+done
--- homeworks/week1/bin/bash/test.sh
+++ homeworks/week1/bin/bash/test.sh
@@ -1,16 +1,16 @@
 # 宣告使用 /bin/bash
 #!/bin/bash
 
+#挑戰題
+
 touch "$1.js";
 echo "檔案建立完成";
 
 
-#  輸入數字n，然後產生n個js資料
+
+
+
+
 # 超級挑戰題
 
 
-for var in list
-
-do
-
-done
