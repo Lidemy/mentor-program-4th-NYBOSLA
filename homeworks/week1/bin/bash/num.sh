0757c4ed3a51bfb027b822ee88700d32703dc7e3
BOSNYLA <shinylincom@gmail.com>
2020-07-15 12:45:43 +0800

    review week1

--- homeworks/week1/bin/bash/num.sh
+++ homeworks/week1/bin/bash/num.sh
@@ -1,12 +1,15 @@
 #  輸入數字n，然後產生n個js資料
+#--下面這個是寫錯的紀錄
+#for number in ${number}.js
+#do
+#touch "${number}.js";
+#echo "檔案建立完成";
+#done
 
 
-for number in ${number}.js
 
+for (( i=1; i<=$1; i=i+1 ))
 do
-
-touch "${number}.js";
-
-echo "檔案建立完成";
-
+  touch "${i}.js";
 done
+echo "檔案建立完成";
