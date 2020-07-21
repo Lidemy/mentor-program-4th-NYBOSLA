daade90c4a6b12875f0fb70ec7d9310b41332928
NYBOSLA <shinylincom@gmail.com>
2020-07-21 12:00:46 +0800

    week4

--- homeworks/week4/hw1.js
+++ homeworks/week4/hw1.js
@@ -5,7 +5,7 @@ const baseUrl = 'https://lidemy-book-store.herokuapp.com';
 request(`${baseUrl}/books?_limit=10`, (error, response, body) => {
 // json
   let data = '';
-  console.log('body:', body);
+  // console.log('body:', body);
   // 把json轉成陣列
   data = JSON.parse(body);
   for (let i = 0; i < data.length; i += 1) {
